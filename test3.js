fdee22e2dfcefa36125a0cd9acbafb7816ff2d41
Alhajideen <salifunurudeen834@gmail.com>
2022-10-04 14:39:05 -0600

    add calculator object with methods

---
+++ test3.js
@@ -0,0 +1,17 @@
+const calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+
+  sub: (a, b) => {
+    return a - b;
+  },
+
+  div: (a, b) => {
+    return a / b;
+  },
+
+  mult: (a, b) => {
+    return a * b;
+  },
+};
